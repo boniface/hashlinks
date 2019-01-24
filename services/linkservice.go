2d820493715a10d50fa70fbcc683e3a5162433ad
boniface <boniface@kabaso.com>
2019-01-24 21:34:18 +0200

    Fixed Feed breaking

--- services/linkservice.go
+++ services/linkservice.go
@@ -8,19 +8,33 @@ import (
 	"hashlinks/repository"
 )
 
+func recoverName() {
+	if r := recover(); r != nil {
+		fmt.Println("recovered from ", r)
+	}
+}
+
 func GetZoneLinks(zone string) domain.Links {
+	linksChannel := make(chan domain.Links)
 	var links domain.Links
 	feeds := repository.GetZoneFeeds(zone)
 	for _, feed := range feeds {
-		links = append(links, getLinks(feed)...)
+		getLinks(feed, linksChannel)
+		links := <-linksChannel
 	}
+
 	return links
 }
 
-func getLinks(feedLink domain.Feed) domain.Links {
+func getLinks(feedLink domain.Feed, linksChannel chan domain.Links) {
+	defer recoverName()
 	var links domain.Links
 	fp := gofeed.NewParser()
-	feed, _ := fp.ParseURL(feedLink.Feedlink)
+	feed, err := fp.ParseURL(feedLink.Feedlink)
+
+	if err != nil {
+		fmt.Println("Feed: ", err)
+	}
 
 	for _, feed := range feed.Items {
 		link := domain.Link{
@@ -35,7 +49,7 @@ func getLinks(feedLink domain.Feed) domain.Links {
 		}
 		links = append(links, link)
 	}
-	return links
+	linksChannel <- links
 }
 
 func hashLink(url string) string {
